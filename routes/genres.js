0fad9b06ad0ae30324053f97e956bf5d3b42e28c
Evans-Asiedu <evansasiedu352@gmail.com>
2025-02-22 14:20:09 +0000

    Add role base authorization

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 const { Genre, validate } = require("../models/genre");
 const Joi = require("joi");
 const mongoose = require("mongoose");
@@ -37,7 +38,7 @@ router.put("/:id", auth, async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", auth, async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre)
