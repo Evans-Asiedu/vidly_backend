86625f1ce72e10e7ea9874eec365d0263cbc7a42
Evans-Asiedu <evansasiedu352@gmail.com>
2025-02-22 13:43:06 +0000

    Add functionality to get current user

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth"); // here auth represent authorization not authentication
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const bcrypt = require("bcrypt");
@@ -6,6 +7,11 @@ const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password"); // this exclude password from the user object that is return
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
