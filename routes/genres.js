b5ef6e0e01fe9056084f7d1332e033d0f151320d
Evans-Asiedu <evansasiedu352@gmail.com>
2025-02-23 21:12:52 +0000

    Refactor to improve handling of rejected promises

--- routes/genres.js
+++ routes/genres.js
@@ -1,29 +1,32 @@
+const asyncMiddleware = require("../middleware/async");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const { Genre, validate } = require("../models/genre");
-const Joi = require("joi");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
+router.get(
+  "/",
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort("name");
     res.send(genres);
-  } catch (ex) {
-    next(ex);
-  }
-});
+  })
+);
 
-router.post("/", auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post(
+  "/",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
 
-  res.send(genre);
-});
+    res.send(genre);
+  })
+);
 
 router.put("/:id", auth, async (req, res) => {
   const { error } = validate(req.body);
