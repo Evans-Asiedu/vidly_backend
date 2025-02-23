45fa90242c0a9d23e9090a04c9fc51a9cad7299c
Evans-Asiedu <evansasiedu352@gmail.com>
2025-02-23 22:45:46 +0000

    Extract the function for connecting to the db into a separate module

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ require("winston-mongodb");
 const config = require("config");
 const Joi = require("joi");
 const joiObjectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 require("./startup/routes")(app);
+require("./startup/db")();
 
 // for handling unhandle and uncaught exceptions
 winston.handleExceptions(
@@ -33,11 +33,6 @@ if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
   process.exit(1);
 }
-
-mongoose
-  .connect("mongodb://localhost/vidlydb")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
