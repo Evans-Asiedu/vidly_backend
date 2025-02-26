22c82d9f8ae481379a986e14d43c8ecac6c0d4fc
Evans-Asiedu <evansasiedu352@gmail.com>
2025-02-26 16:36:05 +0000

    Test routes with parameters

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -9,22 +9,43 @@ describe("/api/genres", () => {
   });
 
   afterEach(async () => {
-    if (server && server.close) {
-      await server.close();
+    if (server) {
+      server.close();
     }
+    await Genre.remove({});
   });
 
   describe("GET /", () => {
     it("should return all genres", async () => {
-      // await Genre.collection.insertMany([
-      //   { name: "genre1" },
-      //   { name: "genre2" },
-      // ]);
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
 
       const res = await request(app).get("/api/genres");
 
       expect(res.status).toBe(200);
-      expect(res.body.length).toBe(4);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return a genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+      await genre.save();
+
+      const res = await request(app).get("/api/genres/" + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+
+    it("should return 404 if invalid id is passed", async () => {
+      const res = await request(app).get("/api/genres/1");
+
+      expect(res.status).toBe(404);
     });
   });
 });
