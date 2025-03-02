c24ba35d8790f077bb21807cedef63db2d0660ca
Evans-Asiedu <evansasiedu352@gmail.com>
2025-03-02 20:15:35 +0000

    Test for movie stock using TDD approach

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,6 +1,7 @@
 const moment = require("moment");
 const request = require("supertest");
 const { Rental } = require("../../models/rental");
+const { Movie } = require("../../models/movie");
 const { User } = require("../../models/user");
 const mongoose = require("mongoose");
 
@@ -11,6 +12,7 @@ describe("/api/returns", () => {
   let customerId;
   let movieId;
   let rental;
+  let movie;
   let token;
 
   const exec = async () => {
@@ -24,6 +26,15 @@ describe("/api/returns", () => {
     customerId = mongoose.Types.ObjectId();
     movieId = mongoose.Types.ObjectId();
     token = new User().generateAuthToken();
+
+    movie = new Movie({
+      _id: movieId,
+      title: "12345",
+      dailyRentalRate: 2,
+      genre: { name: "12345" },
+      numberInStock: 10,
+    });
+    await movie.save();
 
     rental = new Rental({
       customer: {
@@ -45,6 +56,7 @@ describe("/api/returns", () => {
       server.close();
     }
     await Rental.remove({});
+    await Movie.remove({});
   });
 
   it("should return 401 if client is not logged in", async () => {
@@ -110,5 +122,11 @@ describe("/api/returns", () => {
 
     const rentalInDb = await Rental.findById(rental._id);
     expect(rentalInDb.rentalFee).toBe(14);
+  });
+
+  it("should increase the movie stock if input is valid", async () => {
+    const res = await exec();
+    const movieInDb = await Movie.findById(movieId);
+    expect(movieInDb.numberInStock).toBe(movie.numberInStock + 1);
   });
 });
