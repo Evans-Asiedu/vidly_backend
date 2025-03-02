9219f15cb39bf934a69d35e9e7bcec1a5d7b0378
Evans-Asiedu <evansasiedu352@gmail.com>
2025-03-02 19:10:07 +0000

    Test for looking up an object  using TDD approach

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -69,4 +69,12 @@ describe("/api/returns", () => {
 
     expect(res.status).toBe(400);
   });
+
+  it("should return 400 if no rental found for the customer/movie", async () => {
+    await Rental.remove({});
+
+    const res = await exec();
+
+    expect(res.status).toBe(404);
+  });
 });
